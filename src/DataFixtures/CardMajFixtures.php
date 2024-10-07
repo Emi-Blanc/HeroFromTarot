11be42c12fd9af737258731fd62c515717e4ae8e
Emilie Blanckaert <emib44@gmail.com>
2024-10-07 12:49:04 +0200

    Ajout nom numero description dans SQL

--- src/DataFixtures/CardMajFixtures.php
+++ src/DataFixtures/CardMajFixtures.php
@@ -27,136 +27,119 @@ class CardMajFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $cardFou = new CardMaj([
-            'name' => "Le Fou",
-            'number' => "0",
-            'description' => "Une personne libre d'esprit et aventureuse, souvent perçue comme spontanée et créative. Elle est prête à prendre des risques et à explorer l'inconnu avec une curiosité enfantine.",
-        ]);
+        // $cardFou = new CardMaj();
+        // $cardFou->setName("Le Fou");
+        // $cardFou->setNumber("0");
+        // $cardFou->setDescription("Une personne libre d'esprit et aventureuse, souvent perçue comme spontanée et créative. Elle est prête à prendre des risques et à explorer l'inconnu avec une curiosité enfantine.");
 
-        $cardBateleur = new CardMaj([
-            'name' => "Le Bateleur",
-            'number' => "1",
-            'description' => "Une personne créative et charismatique, qui a le pouvoir de transformer ses idées en réalité. Elle est souvent perçue comme une initiatrice, capable d'inspirer et de motiver les autres.",
-        ]);
- 
-        $cardPapesse = new CardMaj([
-            'name' => "La Papesse",
-            'number' => "2",
-            'description' => "Une personne intuitive et sage, qui valorise la connaissance intérieure et l'introspection. Elle est souvent perçue comme mystérieuse et en connexion avec le monde spirituel.",
-        ]);
-
-        $cardImperatrice = new CardMaj([
-            'name' => "L'Impératrice'",
-            'number' => "3",
-            'description' => "Une personne nourrissante et créative, qui valorise la beauté, l'abondance et la nature. Elle est souvent perçue comme une figure maternelle, capable de créer un environnement chaleureux.",
-        ]);
-
-        $cardEmpereur = new CardMaj([
-            'name' => "L'Empereur'",
-            'number' => "4",
-            'description' => "Une personne autoritaire et structurée, qui valorise l'ordre et la stabilité. Elle est souvent perçue comme un leader pragmatique, capable de prendre des décisions importantes.",
-        ]);
-
-        $cardPape = new CardMaj([
-            'name' => "Le Pape",
-            'number' => "5",
-            'description' => "Une personne sage et spirituelle, qui valorise les traditions et les enseignements. Elle est souvent perçue comme une figure d'autorité morale, apportant guidance et soutien.",
-        ]);
-
-        $cardAmoureux = new CardMaj([
-            'name' => "Les Amoureux",
-            'number' => "6",
-            'description' => "Une personne passionnée et équilibrée, qui valorise l'amour et les relations. Elle est souvent perçue comme charismatique et en quête d'harmonie dans ses interactions.",
-        ]);
-
-        $cardChariot = new CardMaj([
-            'name' => "Le Chariot",
-            'number' => "7",
-            'description' => "Une personne déterminée et ambitieuse, qui valorise le succès et la conquête. Elle est souvent perçue comme résiliente, capable de surmonter les obstacles sur son chemin.",
-        ]);
-
-        $cardErmite = new CardMaj([
-            'name' => "L'Ermite'",
-            'number' => "9",
-            'description' => "Une personne introspective et sage, qui valorise la solitude et la recherche de la vérité. Elle est souvent perçue comme un guide, capable d'apporter une perspective profonde.",
-        ]);
-
-
-        $cardRoueFortune = new CardMaj([
-            'name' => "La Roue de la Fortune",
-            'number' => "10",
-            'description' => "Une personne adaptable et ouverte, qui reconnaît les cycles de la vie. Elle est souvent perçue comme optimiste, capable d'accepter le changement et les fluctuations.",
-        ]);
-
-        $cardJustice = new CardMaj([
-            'name' => "La Justice",
-            'number' => "11",
-            'description' => "Une personne équilibrée et juste, qui valorise l'honnêteté et l'équité. Elle est souvent perçue comme un arbitre, capable de prendre des décisions éclairées et impartiales.",
-        ]);
-
-        $cardPendu = new CardMaj([
-            'name' => "Le Pendu",
-            'number' => "12",
-            'description' => "Une personne introspective et détachée, qui valorise la réflexion et le lâcher-prise. Elle est souvent perçue comme sage, capable de voir les choses sous un angle différent.",
-        ]);
-
-        $cardMort = new CardMaj([
-            'name' => "La Mort",
-            'number' => "13",
-            'description' => "Une personne transformée et résiliente, qui valorise la fin d’un cycle et le renouveau. Elle est souvent perçue comme capable de laisser derrière elle ce qui ne sert plus.",
-        ]);
-
-        $cardTemperance = new CardMaj([
-            'name' => "La Tempérance",
-            'number' => "14",
-            'description' => "Une personne équilibrée et harmonieuse, qui valorise la patience et la modération. Elle est souvent perçue comme une médiatrice, capable d'apporter la paix et la tranquillité.",
-        ]);
-
-        $cardDiable = new CardMaj([
-            'name' => "Le Diable",
-            'number' => "15",
-            'description' => "Une personne passionnée et charismatique, qui peut être attirée par les plaisirs matériels et les dépendances. Elle est souvent perçue comme intense, avec des luttes intérieures.",
-        ]);
-
-        $cardTour = new CardMaj([
-            'name' => "La Tour",
-            'number' => "16",
-            'description' => "Une personne en pleine transformation, souvent perçue comme confrontée à des bouleversements et des crises. Elle est capable de faire face à l'imprévu avec courage et détermination.",
-        ]);
-      
-        $cardEtoile = new CardMaj([
-            'name' => "L'Etoile'",
-            'number' => "17",
-            'description' => "Une personne inspirante et optimiste, qui valorise l'espoir et la spiritualité. Elle est souvent perçue comme une guide, capable d'apporter lumière et inspiration aux autres.",
-        ]);
-
-        $cardLune = new CardMaj([
-            'name' => "La Lune",
-            'number' => "18",
-            'description' => "Une ersonne calme et réfléchie, qui valorise l'intuition et les émotions. Elle est souvent perçue comme mystérieuse et apaisante, apportant une aura de sérénité. ",
-        ]);
-
-        $cardSoleil = new CardMaj([
-            'name' => "Le soleil",
-            'number' => "19",
-            'description' => "Une personne joyeuse et énergique, qui valorise la positivité et la vitalité. Elle est souvent perçue comme une source de lumière et de bonheur pour son entourage.",
-        ]);
-
-        $cardJugement = new CardMaj([
-            'name' => "Le Jugement",
-            'number' => "20",
-            'description' => "Une personne introspective et juste, qui valorise la rédemption et la transformation personnelle. Elle est souvent perçue comme un catalyseur de changement et de renouveau.",
-        ]);
+        // $cardBateleur = new CardMaj();
+        // $cardBateleur->setName("Le Bateleur");
+        // $cardBateleur->setNumber("1");
+        // $cardBateleur->setDescription("Une personne créative et charismatique, qui a le pouvoir de transformer ses idées en réalité. Elle est souvent perçue comme une initiatrice, capable d'inspirer et de motiver les autres.");
         
-        $cardMonde = new CardMaj([
-            'name' => "Le Monde",
-            'number' => "21",
-            'description' => "Une personne accomplie et intégrée, qui valorise l'harmonie et l'achèvement. Elle est souvent perçue comme un exemple d'équilibre et de réussite.",
-        ]);
-       
+        // $cardPapesse = new CardMaj();
+        // $cardPapesse->setName("La Papesse");
+        // $cardPapesse->setNumber("2");
+        // $cardPapesse->setDescription("Une personne intuitive et sage, qui valorise la connaissance intérieure et l'introspection. Elle est souvent perçue comme mystérieuse et en connexion avec le monde spirituel.");
+        
+        // $cardImperatrice = new CardMaj();
+        // $cardImperatrice->setName("L'Impératrice");
+        // $cardImperatrice->setNumber("3");
+        // $cardImperatrice->setDescription("Une personne nourrissante et créative, qui valorise la beauté, l'abondance et la nature. Elle est souvent perçue comme une figure maternelle, capable de créer un environnement chaleureux.");
+        
+        // $cardEmpereur = new CardMaj();
+        // $cardEmpereur->setName("L'Empereur");
+        // $cardEmpereur->setNumber("4");
+        // $cardEmpereur->setDescription("Une personne autoritaire et structurée, qui valorise l'ordre et la stabilité. Elle est souvent perçue comme un leader pragmatique, capable de prendre des décisions importantes.");
+        
+        // $cardPape = new CardMaj();
+        // $cardPape->setName("Le Pape");
+        // $cardPape->setNumber("5");
+        // $cardPape->setDescription("Une personne sage et spirituelle, qui valorise les traditions et les enseignements. Elle est souvent perçue comme une figure d'autorité morale, apportant guidance et soutien.");
+        
+        // $cardAmoureux = new CardMaj();
+        // $cardAmoureux->setName("Les Amoureux");
+        // $cardAmoureux->setNumber("6");
+        // $cardAmoureux->setDescription("Une personne passionnée et équilibrée, qui valorise l'amour et les relations. Elle est souvent perçue comme charismatique et en quête d'harmonie dans ses interactions.");
+        
+        // $cardChariot = new CardMaj();
+        // $cardChariot->setName("Le Chariot");
+        // $cardChariot->setNumber("7");
+        // $cardChariot->setDescription("Une personne déterminée et ambitieuse, qui valorise le succès et la conquête. Elle est souvent perçue comme résiliente, capable de surmonter les obstacles sur son chemin.");
+        
+        // $cardForce = new CardMaj();
+        // $cardForce->setName("La Force");
+        // $cardForce->setNumber("8");
+        // $cardForce->setDescription("Une personne courageuse et douce, qui valorise la compassion et la résilience. Elle est souvent perçue comme forte intérieurement, capable de surmonter les défis avec amour et détermination.");
+        
+        // $cardErmite = new CardMaj();
+        // $cardErmite->setName("L'Ermite");
+        // $cardErmite->setNumber("9");
+        // $cardErmite->setDescription("Une personne introspective et sage, qui valorise la solitude et la recherche de la vérité. Elle est souvent perçue comme un guide, capable d'apporter une perspective profonde.");
+        
+        // $cardRoueFortune = new CardMaj();
+        // $cardRoueFortune->setName("La Roue de la Fortune");
+        // $cardRoueFortune->setNumber("10");
+        // $cardRoueFortune->setDescription("Une personne adaptable et ouverte, qui reconnaît les cycles de la vie. Elle est souvent perçue comme optimiste, capable d'accepter le changement et les fluctuations.");
+        
+        // $cardJustice = new CardMaj();
+        // $cardJustice->setName("La Justice");
+        // $cardJustice->setNumber("11");
+        // $cardJustice->setDescription("Une personne équilibrée et juste, qui valorise l'honnêteté et l'équité. Elle est souvent perçue comme un arbitre, capable de prendre des décisions éclairées et impartiales.");
+        
+        // $cardPendu = new CardMaj();
+        // $cardPendu->setName("Le Pendu");
+        // $cardPendu->setNumber("12");
+        // $cardPendu->setDescription("Une personne introspective et détachée, qui valorise la réflexion et le lâcher-prise. Elle est souvent perçue comme sage, capable de voir les choses sous un angle différent.");
+        
+        // $cardMort = new CardMaj();
+        // $cardMort->setName("La Mort");
+        // $cardMort->setNumber("13");
+        // $cardMort->setDescription("Une personne transformée et résiliente, qui valorise la fin d’un cycle et le renouveau. Elle est souvent perçue comme capable de laisser derrière elle ce qui ne sert plus.");
+        
+        // $cardTemperance = new CardMaj();
+        // $cardTemperance->setName("La Tempérance");
+        // $cardTemperance->setNumber("14");
+        // $cardTemperance->setDescription("Une personne équilibrée et harmonieuse, qui valorise la patience et la modération. Elle est souvent perçue comme une médiatrice, capable d'apporter la paix et la tranquillité.");
+        
+        // $cardDiable = new CardMaj();
+        // $cardDiable->setName("Le Diable");
+        // $cardDiable->setNumber("15");
+        // $cardDiable->setDescription("Une personne passionnée et charismatique, qui peut être attirée par les plaisirs matériels et les dépendances. Elle est souvent perçue comme intense, avec des luttes intérieures.");
+        
+        // $cardTour = new CardMaj();
+        // $cardTour->setName("La Tour");
+        // $cardTour->setNumber("16");
+        // $cardTour->setDescription("Une personne en pleine transformation, souvent perçue comme confrontée à des bouleversements et des crises. Elle est capable de faire face à l'imprévu avec courage et détermination.");
+        
+        // $cardEtoile = new CardMaj();
+        // $cardEtoile->setName("L'Etoile");
+        // $cardEtoile->setNumber("17");
+        // $cardEtoile->setDescription("Une personne inspirante et optimiste, qui valorise l'espoir et la spiritualité. Elle est souvent perçue comme une guide, capable d'apporter lumière et inspiration aux autres.");
+        
+        // $cardLune = new CardMaj();
+        // $cardLune->setName("La Lune");
+        // $cardLune->setNumber("18");
+        // $cardLune->setDescription("Une personne calme et réfléchie, qui valorise l'intuition et les émotions. Elle est souvent perçue comme mystérieuse et apaisante, apportant une aura de sérénité.");
+        
+        // $cardSoleil = new CardMaj();
+        // $cardSoleil->setName("Le Soleil");
+        // $cardSoleil->setNumber("19");
+        // $cardSoleil->setDescription("Une personne joyeuse et énergique, qui valorise la positivité et la vitalité. Elle est souvent perçue comme une source de lumière et de bonheur pour son entourage.");
+        
+        // $cardJugement = new CardMaj();
+        // $cardJugement->setName("Le Jugement");
+        // $cardJugement->setNumber("20");
+        // $cardJugement->setDescription("Une personne introspective et juste, qui valorise la rédemption et la transformation personnelle. Elle est souvent perçue comme un catalyseur de changement et de renouveau.");
+        
+        // $cardMonde = new CardMaj();
+        // $cardMonde->setName("Le Monde");
+        // $cardMonde->setNumber("21");
+        // $cardMonde->setDescription("Une personne accomplie et intégrée, qui valorise l'harmonie et l'achèvement. Elle est souvent perçue comme un exemple d'équilibre et de réussite.");
+        
 
-        $manager->persist($cardFou, $cardBateleur, );
- 
-        $manager->flush();
+        // $manager->persist($cardFou, $cardBateleur, $cardPapesse, $cardImperatrice, $cardEmpereur, $cardPape, $cardAmoureux, $cardChariot, $cardForce, $cardErmite, $cardRoueFortune, $cardJustice, $cardPendu, $cardMort, $cardTemperance, $cardDiable, $cardTour, $cardEtoile, $cardLune, $cardSoleil, $cardJugement, $cardMonde);
+
+        // $manager->flush();
     }
 }
