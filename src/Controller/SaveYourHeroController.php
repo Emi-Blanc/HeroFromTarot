7763417af1df4112be2296303206cebd9eba9278
Emilie Blanckaert <emib44@gmail.com>
2024-10-18 16:23:15 +0200

    Version incroyable! Sauf pour la carte mineure qui change lors de la sauvegarde nondidju

--- src/Controller/SaveYourHeroController.php
+++ src/Controller/SaveYourHeroController.php
@@ -25,9 +25,6 @@ class SaveYourHeroController extends AbstractController
         EntityManagerInterface $entityManager
     ): JsonResponse {
 
-
-
-
         if ($request->isMethod('POST')) {
             $data = json_decode($request->getContent(), true);
 
@@ -37,6 +34,11 @@ class SaveYourHeroController extends AbstractController
             $hero = new Hero();
             $hero->setName($data['name']);
 
+            // dd($data);
+
+            $hero->setCardMaj($cardMajRepository->find($data['cartes'][0]));
+            $hero->setCardRoy($cardRoyRepository->find($data['cartes'][1]));
+            $hero->setCardMin($cardMinRepository->find($data['cartes'][2]));
 
             // Associer l'utilisateur connecté au héros
             $user = $this->getUser();
@@ -85,20 +87,34 @@ class SaveYourHeroController extends AbstractController
             throw $this->createNotFoundException('Héros non trouvé');
         }
 
-        dd($hero->getCardMajs()[0]);
+        // dd($hero->getCardMajs()[0]);
 
         error_log("Héros trouvé: " . $hero->getName());
 
         // Vérifiez les cartes
-        error_log("Cartes majeures: " . count($hero->getCardMajs()));
-        error_log("Cartes royales: " . count($hero->getCardRoys()));
-        error_log("Cartes mineures: " . count($hero->getCardMins()));
+        // error_log("Cartes majeures: " . count($hero->getCardMaj()));
+        // error_log("Cartes royales: " . count($hero->getCardRoy()));
+        // error_log("Cartes mineures: " . count($hero->getCardMin()));
+        if ($hero->getCardMaj()) {
+            error_log("Carte majeure présente : " . $hero->getCardMaj()->getName());
+        } else {
+            error_log("Pas de carte majeure.");
+        }
+        if ($hero->getCardRoy()) {
+            error_log("Carte majeure présente : " . $hero->getCardRoy()->getNom());
+        } else {
+            error_log("Pas de carte majeure.");
+        }if ($hero->getCardMin()) {
+            error_log("Carte majeure présente : " . $hero->getCardMin()->getNumero());
+        } else {
+            error_log("Pas de carte majeure.");
+        }
 
         return $this->render('save_your_hero/detail.html.twig', [
             'hero' => $hero,
-            'cardMajs' => $hero->getCardMajs(),
-            'cardRoys' => $hero->getCardRoys(),
-            'cardMins' => $hero->getCardMins(),
+            'cardMaj' => $hero->getCardMaj(),
+            'cardRoy' => $hero->getCardRoy(),
+            'cardMin' => $hero->getCardMin(),
         ]);
     }
 }
